0a0698ab20af70771441ed18037f01b7088f0328
kimkh05 <nestjs01@naver.com>
2023-02-01 23:32:20 +0900

    feat: init theme color

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,5 +1,22 @@
 const theme = {
-  color: {},
+  color: {
+    main01: "#FFD12D",
+    main02: "#FFE380",
+    main03: "#FFF8E1",
+    gray100: "#FDFDFD",
+    gray200: "#EDEDED",
+    gray300: "#D3D3D3",
+    gray400: "#BDBDBD",
+    gray500: "#A7A7A7",
+    gray600: "#929292",
+    gray700: "#7C7C7C",
+    gray800: "#505050",
+    gray900: "#343434",
+    background: "#FFFBFB",
+    error: "#DC3035",
+    check: "#04DF00",
+    focus: "#006EFF",
+  },
   font: {},
 } as const;
 
