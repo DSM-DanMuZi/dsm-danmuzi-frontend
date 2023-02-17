f7b8fdc03930eebe6681f2f7a443c670c28b4755
kimkh05 <nestjs01@naver.com>
2023-02-17 15:35:45 +0900

    fix: add import

--- styles/index.tsx
+++ styles/index.tsx
@@ -1,9 +1,10 @@
 import { Global, ThemeProvider } from "@emotion/react";
 import { theme } from "./theme";
 import global from "./global";
+import { ReactNode } from "react";
 
 interface PropsType {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
 export const StyleProvider = ({ children }: PropsType) => (
