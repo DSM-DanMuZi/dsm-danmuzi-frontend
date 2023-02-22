4ef5861afe98a7d19d0f08d1ed1f59c3bdc9ddb0
yoon-ho7 <suho0317@gmail.com>
2023-02-22 20:52:01 +0900

    feat: make recommend book component

---
+++ components/common/recommend/book/index.tsx
@@ -0,0 +1,52 @@
+import styled from "@emotion/styled";
+import Image from "next/image";
+
+interface PropsType {
+  image_url: string;
+  title: string;
+  name: string;
+  type: string;
+}
+
+const RecommendBook = ({ image_url, title, name, type }: PropsType) => {
+  return (
+    <_Wrapper>
+      <_Image src={image_url} alt="image" />
+      <_Type>{type}</_Type>
+      <_Title>{title}</_Title>
+      <_Name>{name}</_Name>
+    </_Wrapper>
+  );
+};
+
+export default RecommendBook;
+
+const _Wrapper = styled.div`
+  width: 320px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const _Image = styled(Image)`
+  width: 320px;
+  height: 400px;
+  margin-bottom: 18px;
+`;
+
+const _Type = styled.span`
+  ${({ theme }) => theme.font.body4}
+  color: ${({ theme }) => theme.color.gray000};
+  margin-bottom: 6px;
+`;
+
+const _Title = styled.span`
+  ${({ theme }) => theme.font.title3};
+  color: ${({ theme }) => theme.color.gray000};
+  margin-bottom: 12px;
+`;
+
+const _Name = styled.span`
+  ${({ theme }) => theme.font.body4};
+  line-height: 44px;
+  color: ${({ theme }) => theme.color.gray000};
+`;
