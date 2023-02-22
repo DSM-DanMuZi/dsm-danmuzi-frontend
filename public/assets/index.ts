68f4431e66b674d1f458cf39a32c9f087a32a2fe
kimkh05 <nestjs01@naver.com>
2023-02-22 20:00:06 +0900

    feat: init image

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,3 +1,4 @@
 export { default as GithubLogo } from "./github.svg";
 export { default as SearchButton } from "./search-button.svg";
 export { default as Logo } from "./main-logo.svg";
+export { default as LoungeDevelop } from "./lounge_develop.png";
