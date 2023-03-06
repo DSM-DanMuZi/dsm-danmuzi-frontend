64828716456149b04d95c71b72ae2c36e6ea892a
hwangjinseong <imhjs2023@gmail.com>
2023-03-06 22:30:26 +0900

    fix: style

--- styles/button.tsx
+++ styles/button.tsx
@@ -6,6 +6,7 @@ import { ReactElement, ReactNode } from "react";
 interface ButtonStyle {
   buttonColor: keyOfColorType;
   fontColor: keyOfColorType;
+  inner: string;
 }
 
 interface ButtonProps
@@ -14,15 +15,8 @@ interface ButtonProps
   children: ReactNode;
 }
 
-const Button = ({
-  children,
-  ...rest
-}: ButtonProps): ReactElement => {
-  return (
-    <ButtonStyled {...rest}>
-      {children}
-    </ButtonStyled>
-  );
+const Button = ({ children, ...rest }: ButtonProps): ReactElement => {
+  return <ButtonStyled {...rest}>{children}</ButtonStyled>;
 };
 
 export default Button;
@@ -30,10 +24,20 @@ export default Button;
 const ButtonStyled = styled.button<ButtonStyle>`
   padding: 8px 24px;
   border-radius: 10px;
+  line-height: 32px;
+  transition: 0.5s;
   ${({ theme }) => theme.font.body3}
   cursor: pointer;
   ${({ buttonColor = "white", fontColor = "white" }) => css`
     background-color: ${color[buttonColor]};
     color: ${color[fontColor]};
   `}
+  background-color: ${({ theme, inner }) =>
+    inner === "로그인" ? theme.color.main01 : theme.color.gray100};
+  color: ${({ theme, inner }) =>
+    inner === "로그인" ? theme.color.gray100 : theme.color.gray800};
+  :hover {
+    background-color: ${({ theme, inner }) =>
+      inner === "문의하기" ? theme.color.gray200 : theme.color.main02};
+  }
 `;
