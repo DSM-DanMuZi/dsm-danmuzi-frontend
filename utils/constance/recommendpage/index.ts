da14eb3eea2795f63391ac04242bcee0b3216e75
hwangjinseong <imhjs2023@gmail.com>
2023-03-07 17:26:31 +0900

    fix: bug

--- utils/constance/recommendpage/index.ts
+++ utils/constance/recommendpage/index.ts
@@ -1,27 +1,27 @@
-import { RecommendBookType, RecommendDeveloperType, RecommendDesignerType, RecommendLectureType } from "@/utils/types/recommendpage";
+import { RecommendationType } from "@/utils/types/recommendpage";
 
-export const RecommendBookDummy: RecommendBookType[] = new Array(4).fill({
+export const RecommendBookDummy: RecommendationType[] = new Array(4).fill({
   image_url: "",
   title: "UX 디자이너로 일하고 있습니다",
   name: "Product Designer 장석연",
   type: "디자인",
 }); 
 
-export const RecommendDeveloperDummy: RecommendDeveloperType[] = new Array(6).fill({
+export const RecommendDeveloperDummy: RecommendationType[] = new Array(6).fill({
   image_url: "",
   name: "니꼴라스",
   text: "안녕하세요 니꼴라스,입니다.",
   type: "꿀팁",
 }); 
 
-export const RecommendDesignerDummy: RecommendDesignerType[] = new Array(6).fill({
+export const RecommendDesignerDummy: RecommendationType[] = new Array(6).fill({
   image_url: "",
   name: "Madia",
   text: "대한민국 No.1 Designer",
   type: "UX/UI",
 }); 
 
-export const RecommendLectureDummy: RecommendLectureType[] = new Array(3).fill({
+export const RecommendLectureDummy: RecommendationType[] = new Array(3).fill({
   image_url: "",
   name: "준코바",
   title: "준코바 제자들의 모션그래픽수준 실화..?",
--- utils/types/recommendpage/index.ts
+++ utils/types/recommendpage/index.ts
@@ -1,27 +1,7 @@
-export interface RecommendBookType {
-  image_url: string;
-  title: string;
-  name: string;
-  type: string;
-}
-
-export interface RecommendDeveloperType {
+export interface RecommendationType {
   image_url: string;
   name: string;
-  text: string;
   type: string;
-}
-
-export interface RecommendDesignerType {
-  image_url: string;
-  name: string;
-  text: string;
-  type: string;
-}
-
-export interface RecommendLectureType {
-  image_url: string;
-  name: string;
-  title: string;
-  type: string;
+  title?: string;
+  text?: string;
 }
