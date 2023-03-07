d11b55be171c4f2ffa2eff6d2f46b7dd46ef33ea
hwangjinseong <imhjs2023@gmail.com>
2023-03-07 17:26:22 +0900

    fix: bug

--- pages/recommendedpage.tsx
+++ pages/recommendedpage.tsx
@@ -6,8 +6,15 @@ import RecommendBook from "@/components/common/recommend/book";
 import RecommendDeveloper from "@/components/common/recommend/developer";
 import RecommendDesigner from "@/components/common/recommend/designer";
 import RecommendLecture from "@/components/common/recommend/lecture";
-import { RecommendBookDummy, RecommendDeveloperDummy, RecommendDesignerDummy, RecommendLectureDummy } from "@/utils/constance/recommendpage";
-import { RecommendBookType, RecommendDeveloperType, RecommendDesignerType, RecommendLectureType } from "@/utils/types/recommendpage";
+import {
+  RecommendBookDummy,
+  RecommendDeveloperDummy,
+  RecommendDesignerDummy,
+  RecommendLectureDummy,
+} from "@/utils/constance/recommendpage";
+import {
+  RecommendationType
+} from "@/utils/types/recommendpage";
 
 const RecommendedPage = () => {
   return (
@@ -19,7 +26,7 @@ const RecommendedPage = () => {
       <ItemWrapper>
         <Text>고수들이 읽는 책😎</Text>
         <BookItemWrapper>
-          {RecommendBookDummy.map((element: RecommendBookType, idx: number) => (
+          {RecommendBookDummy.map((element: RecommendationType, idx: number) => (
             <RecommendBook
               key={idx}
               image_url={element.image_url}
@@ -33,43 +40,49 @@ const RecommendedPage = () => {
       <ItemWrapper>
         <Text>개발 관련 유튜버 💻</Text>
         <BookItemWrapper>
-          {RecommendDeveloperDummy.map((element: RecommendDeveloperType, idx: number) => (
-            <RecommendDeveloper
-              key={idx}
-              image_url={element.image_url}
-              type={element.type}
-              text={element.text}
-              name={element.name}
-            />
-          ))}
+          {RecommendDeveloperDummy.map(
+            (element: RecommendationType, idx: number) => (
+              <RecommendDeveloper
+                key={idx}
+                image_url={element.image_url}
+                type={element.type}
+                text={element.text}
+                name={element.name}
+              />
+            )
+          )}
         </BookItemWrapper>
       </ItemWrapper>
       <ItemWrapper>
         <Text>디자인 유튜버 ✍️</Text>
         <BookItemWrapper>
-          {RecommendDesignerDummy.map((element: RecommendDesignerType, idx: number) => (
-            <RecommendDesigner
-              key={idx}
-              image_url={element.image_url}
-              type={element.type}
-              text={element.text}
-              name={element.name}
-            />
-          ))}
+          {RecommendDesignerDummy.map(
+            (element: RecommendationType, idx: number) => (
+              <RecommendDesigner
+                key={idx}
+                image_url={element.image_url}
+                type={element.type}
+                text={element.text}
+                name={element.name}
+              />
+            )
+          )}
         </BookItemWrapper>
       </ItemWrapper>
       <ItemWrapper>
         <Text>도움되는 영상 🎥</Text>
         <BookItemWrapper>
-          {RecommendLectureDummy.map((element: RecommendLectureType, idx: number) => (
-            <RecommendLecture
-              key={idx}
-              image_url={element.image_url}
-              type={element.type}
-              title={element.title}
-              name={element.name}
-            />
-          ))}
+          {RecommendLectureDummy.map(
+            (element: RecommendationType, idx: number) => (
+              <RecommendLecture
+                key={idx}
+                image_url={element.image_url}
+                type={element.type}
+                title={element.title}
+                name={element.name}
+              />
+            )
+          )}
         </BookItemWrapper>
       </ItemWrapper>
       <Footer />
