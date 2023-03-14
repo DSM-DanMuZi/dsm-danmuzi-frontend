23dec974e488b1c58c06d5dca182d296ed66de83
hwangjinseong <imhjs2023@gmail.com>
2023-03-14 10:23:43 +0900

    fix: bug

--- components/advertisement/advertisement3.tsx
+++ components/advertisement/advertisement3.tsx
@@ -1,10 +1,8 @@
 import styled from "@emotion/styled";
-import NumberButton from "./numberbutton";
 
 const advertisement3 = () => {
   return (
     <Wrapper>
-      <NumberButton currentNum="3" maximumNum="3" />
     </Wrapper>
   )
 }
@@ -15,5 +13,5 @@ const Wrapper = styled.div`
   width: 100%;
   height: 500px;
   margin-top: 80px;
-  background-color: ${({theme}) => theme.color.gray400};
+  background-color: ${({theme}) => theme.color.gray200};
 `;
