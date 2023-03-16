fa4716a2e8b1e55c08dd9a4f15c6305c836cb251
yoon-ho7 <suho0317@gmail.com>
2023-03-16 23:24:31 +0900

    feat : each study page

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -2,4 +2,5 @@ export { default as GithubLogo } from "./github.svg";
 export { default as SearchButton } from "./search-button.svg";
 export { default as Logo } from "./main-logo.svg";
 export { default as LoungeDevelop } from "./lounge_develop.png";
-export { default as LoginMain } from './login_image.svg';
+export { default as LoginMain } from "./login_image.svg";
+export { default as StudyLike } from "./study_like.png";
