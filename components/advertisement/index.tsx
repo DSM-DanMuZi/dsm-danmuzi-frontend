3513f5fc46ed6ec5e3b1bda61b086f4e8d1389ba
kimkh05 <nestjs01@naver.com>
2023-03-18 13:22:53 +0900

    feat: tie import components

---
+++ components/advertisement/index.tsx
@@ -0,0 +1,9 @@
+import Advertisement from "./advertisement1";
+import Advertisement2 from "./advertisement2";
+import Advertisement3 from "./advertisement3";
+
+export const AdvertisementList = {
+  Advertisement,
+  Advertisement2,
+  Advertisement3,
+};
